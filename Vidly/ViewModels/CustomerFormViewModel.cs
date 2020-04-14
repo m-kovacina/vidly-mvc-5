6bb2513b5e4efe33fb90a33e4ada96b001a1a34e
Marko Kovačina <m-kovacina@users.noreply.github.com>
2020-04-15 01:41:45 +0200

    Customer form added
    Customer form added for creation and editing.

---
+++ Vidly/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MenMembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
